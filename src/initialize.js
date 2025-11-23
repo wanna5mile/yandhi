7ad86fb16d58d3c386fa474d0ef49ee4442478b6
R5 <kitloverbs90210@gmail.com>
2025-11-22 22:11:30 -0600

    Update initialize.js

--- src/initialize.js
+++ src/initialize.js
@@ -1,22 +1,22 @@
 import { MusicPlayer } from "./audio/player.js";
 
 const playlist = [
-  "./assets/music/bye-bye-baby.mp3",
-  "./assets/music/the-chakra.mp3",
-  "./assets/music/alien.mp3",
-  "./assets/music/new-body.mp3",
-  "./assets/music/xxx.mp3",
-  "./assets/music/sky-city.mp3",
-  "./assets/music/brothers.mp3",
-  "./assets/music/the-garden.mp3",
-  "./assets/music/we-free.mp3",
-  "./assets/music/we-got-love.mp3",
-  "./assets/music/cash-to-burn.mp3",
-  "./assets/music/hurricane.mp3",
-  "./assets/music/law-of-attraction.mp3",
-  "./assets/music/last-name.mp3",
-  "./assets/music/spread-your-wings.mp3",
-  "./assets/music/end-of-it.mp3"
+  "./assets/music/bye-bye-baby.mp3"
+//  "./assets/music/the-chakra.mp3",
+//  "./assets/music/alien.mp3",
+//  "./assets/music/new-body.mp3",
+//  "./assets/music/xxx.mp3",
+//  "./assets/music/sky-city.mp3",
+//  "./assets/music/brothers.mp3",
+//  "./assets/music/the-garden.mp3",
+//  "./assets/music/we-free.mp3",
+//  "./assets/music/we-got-love.mp3",
+//  "./assets/music/cash-to-burn.mp3",
+//  "./assets/music/hurricane.mp3",
+//  "./assets/music/law-of-attraction.mp3",
+//  "./assets/music/last-name.mp3",
+//  "./assets/music/spread-your-wings.mp3",
+//  "./assets/music/end-of-it.mp3"
 ];
 
 const player = new MusicPlayer(playlist);
